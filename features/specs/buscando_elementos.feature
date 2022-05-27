b68d76aeda2e29d833fb98bf302675c462f00d23
Vitor Hugo 1973 <devechi.vitor@gmail.com>
2022-05-27 17:06:52 -0300

    first commit

---
+++ features/specs/buscando_elementos.feature
@@ -0,0 +1,10 @@
+#language: pt
+#encoding: utf-8
+
+
+@buscando_elementos
+Funcionalidade: Buscar elementos
+
+Cenario: Buscar elementos com sucesso
+Quando acesso a url botoes
+Entao verifico se encontrei os elementos
---
+++ features/step_definitions/buscando_elementos.rb
@@ -0,0 +1,25 @@
+Quando('acesso a url botoes') do
+    visit ('buscaelementos/botoes')
+    sleep 5
+  end
+
+  Entao('verifico se encontrei os elementos') do
+
+    page.all('.btn-floating')
+    find('#teste').click
+    find('.btn-floating').double_click  #ok
+    find('a.waves-teal:nth-child(2)').click #ok
+    find('.btn.red').click #ok Botão Voltar
+
+    #{}find('a[onclick="ativaedesativa3()"]').click # ok
+    #{}find('#teste')  # ok
+    #{}find_by_id('teste')
+    #{}find_button(class: 'btn waves-light') # ok
+    #find_link(href:"https://www.youtube.com/channel/UCp554v_is_ZzjzSmAQyFfAA").click  # Elemento link
+    #{}find_link(href: 'https://medium.com/automa%C3%A7%C3%A3o-com-batista').click #ok
+    #{}visit 'https://medium.com/automa%C3%A7%C3%A3o-com-batista'
+    #{}find('a.button--primary:nth-child(1)').click #ok
+    sleep 5
+    #{}find('button.button--large:nth-child(1) > span:nth-child(2)').click  # ok
+    #{}find('#identifierId').set('vitor.devechi@gmail.com') #ok
+  end
