83b8650f00aa1f4e98a849c209a9387b5e1732e6
Vitor1973 <devechi.vitor@gmail.com>
2022-09-02 22:33:43 -0300

    Atualização

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,8 @@
 require "capybara/cucumber"
-require "capybara/dsl"
 require "capybara/rspec/matchers"
 require "selenium-webdriver"
 require "webdrivers/chromedriver" ## Foi a solução de um grande problema #ChromeWebdriver versão desatualizada#
-require "site_prism"
-require "byebug"
+
 
 Capybara.configure do |config| ## do(Faz)
 config.default_driver = :selenium_chrome
